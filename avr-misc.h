67b3ee8a5035fb7b2d76ddea67000f630997c412
Anton Rezanov <ton@specadmin.ru>
2020-05-19 20:41:07 +0300

    A simple conversion to hex string
    Converts any number type to hexadecimal string

--- avr-misc.h
+++ avr-misc.h
@@ -2,6 +2,7 @@
 #define AVR_MISC_H
 #define LIB_AVR_MISC
 //-----------------------------------------------------------------------------
+#include <stdlib.h>
 #include "macro.h"
 #include "avr-common.h"
 #include "avr-interrupts.h"
@@ -22,5 +23,22 @@ private:
     BYTE m_count;
     BYTE* m_buf;
 };
+//-----------------------------------------------------------------------------
+template <typename Type>
+char* toHexString(Type value)
+{
+    BYTE count = sizeof(Type) * 2;
+    BYTE octet;
+    char* buf = (char*) malloc(count + 1);
+    buf[count] = '\0';
+    while(count)
+    {
+        octet = value & 0x0F;
+        buf[count - 1] = octet > 9 ? 'A' + octet - 10 : '0' + octet;
+        value >>= 4;
+        count--;
+    }
+    return buf;
+}
 //-----------------------------------------------------------------------------
 #endif
